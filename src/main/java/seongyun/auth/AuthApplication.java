5df5e6f9b244ce0669f42f3060eb95e22ac4f666
lsy96999 <lsy96999@gmail.com>
2023-02-25 01:37:24 +0900

    f

--- src/main/java/seongyun/auth/AuthApplication.java
+++ src/main/java/seongyun/auth/AuthApplication.java
@@ -2,7 +2,17 @@ package seongyun.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class AuthApplication {
 
@@ -10,4 +20,11 @@ public class AuthApplication {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
+	@Bean
+	public RouterFunction<ServerResponse> sample(){
+		return RouterFunctions.route(RequestPredicates.GET("/sample"), (r)->{
+			log.debug("hi");
+			return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue("sample"));
+		});
+	}
 }
