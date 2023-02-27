ae0a114be9a76943bf99d4000cd940927311b996
lsy96999 <lsy96999@gmail.com>
2023-02-27 16:55:45 +0900

    docTest samle

--- src/test/java/seongyun/auth/_sample/router/SampleRouterTest.java
+++ src/test/java/seongyun/auth/_sample/router/SampleRouterTest.java
@@ -3,7 +3,11 @@ package seongyun.auth._sample.router;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.core.publisher.Mono;
+import seongyun.auth._sample.domain.entity.Sample;
 @SpringBootTest
 @ExtendWith({RestDocumentationExtension.class})
 @ActiveProfiles(profiles = {"test"})
@@ -29,16 +35,99 @@ public class SampleRouterTest {
 	void setUp(ApplicationContext applicationContext, RestDocumentationContextProvider restDocumentation) {
 		this.webTestClient = WebTestClient.bindToApplicationContext(applicationContext)
 				.configureClient()
-				.filter(documentationConfiguration(restDocumentation))
+				.baseUrl("https://auth.seongyun.i234.me")
+				.filter(documentationConfiguration(restDocumentation)
+							.operationPreprocessors()
+							.withResponseDefaults(prettyPrint()))
 				.build();
 	}
 	
-//	@Test
+	@Test
 	public void getSamples() {
-		FieldDescriptor[] sample = new FieldDescriptor[] { fieldWithPath("sampleSn").description("샘플SN"),
-				fieldWithPath("sampleNm").description("샘플이름") };
-		this.webTestClient.get().uri("/sample").exchange().expectStatus().isOk()
-		.expectBody().consumeWith(document("sample",
-				responseFields(fieldWithPath("[]").description("arr")).andWithPrefix("[].", sample)));
+		this.webTestClient.get().uri("/api/sample")//.accept(MediaType.APPLICATION_JSON)
+		.exchange()//.expectStatus().isOk()
+		.expectBody().consumeWith(
+					document("samples",
+						responseFields(
+								fieldWithPath("code").description("코드"),
+								fieldWithPath("message").description("메시지"),
+								fieldWithPath("data[]").description("arr"),
+								fieldWithPath("data[].sampleSn").description("샘플SN"),
+								fieldWithPath("data[].sampleNm").description("샘플명")
+								)
+						)
+					);
+	}
+	
+	@Test
+	public void getSample() {
+		this.webTestClient.get().uri("/api/sample/{sn}", 901).exchange()
+		.expectBody().consumeWith(
+					document("sample",
+						pathParameters(parameterWithName("sn").description("요청샘플SN")),
+						responseFields(
+								fieldWithPath("code").description("코드"),
+								fieldWithPath("message").description("메시지"),
+								fieldWithPath("data.sampleNm").description("샘플명"),
+								fieldWithPath("data.sampleSn").description("샘플SN"))
+					)
+				);
+	}
+	
+	@Test
+	public void saveSample() {
+		Sample s = new Sample();
+		s.setSampleNm("sampleNAME");
+		this.webTestClient.post().uri("/api/sample")
+		.body(Mono.just(s), Sample.class)
+		.exchange()
+		.expectBody().consumeWith(
+					document("save",
+							requestFields(
+									fieldWithPath("sampleSn").ignored(),
+									fieldWithPath("sampleNm").description("샘플명")),
+							responseFields(fieldWithPath("code").description("코드"),
+									fieldWithPath("message").description("메시지"),
+									fieldWithPath("data.sampleNm").description("샘플명"),
+									fieldWithPath("data.sampleSn").description("샘플SN"))
+					)
+					
+				);
+	}
+	
+	@Test
+	public void updateSample() {
+		Sample s = new Sample();
+		s.setSampleSn(900L);
+		s.setSampleNm("sampleUpdate");
+		this.webTestClient.put().uri("/api/sample")
+		.body(Mono.just(s), Sample.class)
+		.exchange()
+		.expectBody().consumeWith(
+					document("update",
+							requestFields(
+									fieldWithPath("sampleSn").description("샘플SN"),
+									fieldWithPath("sampleNm").description("샘플명")),
+							responseFields(fieldWithPath("code").description("코드"),
+									fieldWithPath("message").description("메시지"),
+									fieldWithPath("data.sampleNm").description("샘플명"),
+									fieldWithPath("data.sampleSn").description("샘플SN"))
+					)
+					
+				);
+	}
+	
+	@Test
+	public void deleteSample() {
+		this.webTestClient.delete().uri("/api/sample/{sn}", 897).exchange()
+		.expectBody().consumeWith(
+					document("delete",
+						pathParameters(parameterWithName("sn").description("요청샘플SN")),
+						responseFields(
+								fieldWithPath("code").description("코드"),
+								fieldWithPath("message").description("메시지"),
+								fieldWithPath("data").description("_"))
+					)
+				);
 	}
 }
