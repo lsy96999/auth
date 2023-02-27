1ea831aa7d3b7870af8f18b11e39d275c873cedb
lsy96999 <lsy96999@gmail.com>
2023-02-27 20:36:25 +0900

    sql admin, adminrole, commoncode, create and inset

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,12 +6,34 @@
 - insert @2
 
 ## 20220226
+> 공통코드 입력, 어드민 추가
+- delete @3 공통코드
+- insert @4 공통코드
+- delete @5 어드민
+- insert @6 어드민
+- delete @7 어드민 권한
+- insert @8 어드민 권한
 */
-
+---------
 --@1
 delete from tb_sample;
-
 --@2
 insert into tb_sample(sample_nm) values ('hi');
 insert into tb_sample(sample_nm) values ('hi2');
 insert into tb_sample(sample_nm) values ('hi3');
+----------
+--@3
+delete from tb_common_code;
+--@4
+insert into tb_common_code(code_id, code_value, code_nm, use_yn, frst_regist_dt, last_mdfied_dt, frst_regist_user_sn, last_mdfied_user_sn) values ('TK_ADMIN_STTUS_CODE', 'LIVE', '가입', 'Y', now(), now(), 1, 1);
+insert into tb_common_code(code_id, code_value, code_nm, use_yn, frst_regist_dt, last_mdfied_dt, frst_regist_user_sn, last_mdfied_user_sn) values ('TK_ADMIN_STTUS_CODE', 'DEAD', '탈퇴', 'Y', now(), now(), 1, 1);
+insert into tb_common_code(code_id, code_value, code_nm, use_yn, frst_regist_dt, last_mdfied_dt, frst_regist_user_sn, last_mdfied_user_sn) values ('TK_ADMIN_ROLE_CODE', 'ROLE_SUPER', '최고권한', 'Y', now(), now(), 1, 1);
+--@5
+drop table tb_tk_admin;
+--@6
+insert into tb_tk_admin (admin_sn, admin_nm, admin_mail, admin_pw, tk_admin_sttus_code, use_yn, frst_regist_dt, last_mdfied_dt, frst_regist_user_sn, last_mdfied_user_sn) values(1, 'ADMIN', 'ADMIN', 'ADMIN', 'LIVE', 'Y', now(), now(), 1, 1);
+--@7
+drop table tb_tk_admin_role;
+--@8
+insert into tb_tk_admin_role(admin_sn, tk_admin_role_code, use_yn, frst_regist_dt, last_mdfied_dt, frst_regist_user_sn, last_mdfied_user_sn) values(1, 'ROLE_SUPER', 'Y', now(), now(), 1, 1);
+----------
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,16 +3,63 @@
 ## 20220225
 > test를 위한 sample 테이블 및 시퀀스 생성
 - [C, T] tb_sample @ts
-- [C, S] seq_tb_sample 2ss
+- [C, S] seq_tb_sample @ss
 
-## 20220226
+## 20220227
+> 공통 코드, 토큰 어드민, 토큰 어드민 권한 추가
+- [C, T] tb_tk_admin @tta
+- [C, S] seq_tb_tk_admin @sta
+- [C, T] tb_common_code @tcc
+- [C, T] tb_tk_admin_role @ttar
 */
-
+----------
 --@ss
 CREATE SEQUENCE IF NOT EXISTS SEQ_TB_SAMPLE;
-
 --@ts
 CREATE TABLE IF NOT EXISTS TB_SAMPLE(
     SAMPLE_SN int default nextval('SEQ_TB_SAMPLE'),
 	SAMPLE_NM varchar(255)
 );
+----------
+--@tta
+create table IF NOT EXISTS tb_tk_admin(
+	admin_sn numeric(10) primary key default nextval('SEQ_TB_TK_ADMIN'),
+	admin_nm varchar(100),
+	admin_mail varchar(100) unique,
+	admin_pw varchar(100),
+	tk_admin_sttus_code varchar(100),
+	use_yn char(1),
+	frst_regist_dt timestamp,
+	last_mdfied_dt timestamp,
+	frst_regist_user_sn numeric(10),
+	last_mdfied_user_sn numeric(10)
+);
+--@sta
+create sequence IF NOT EXISTS seq_tb_tk_admin;
+--@tcc
+create table tb_common_code(
+	code_id varchar(100),
+	code_value varchar(100),
+	code_nm varchar(100),
+	code_desc varchar(200),
+	code_group varchar(50),
+	sort_ordr numeric(10),
+	use_yn char(1),
+	frst_regist_dt timestamp,
+	last_mdfied_dt timestamp,
+	frst_regist_user_sn numeric(10),
+	last_mdfied_user_sn numeric(10),
+	primary key (code_id, code_value)
+);
+--@ttar
+create table IF NOT EXISTS tb_tk_admin_role(
+	admin_sn numeric(10),
+	tk_admin_role_code varchar(100),
+	use_yn char(1),
+	frst_regist_dt timestamp,
+	last_mdfied_dt timestamp,
+	frst_regist_user_sn numeric(10),
+	last_mdfied_user_sn numeric(10),
+	primary key (admin_sn, tk_admin_role_code)
+);
+----------
