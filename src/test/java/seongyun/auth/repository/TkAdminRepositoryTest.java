0bfac2ca15129129ec9c1948057f2086c3871119
lsy96999 <lsy96999@gmail.com>
2023-03-05 19:28:54 +0900

    stepverifier test

--- src/test/java/seongyun/auth/repository/TkAdminRepositoryTest.java
+++ src/test/java/seongyun/auth/repository/TkAdminRepositoryTest.java
@@ -1,12 +1,28 @@
 package seongyun.auth.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import seongyun.auth.domain.entity.TkAdmin;
-import seongyun.auth.domain.entity.TkAdminRole;
+import org.springframework.test.context.ActiveProfiles;
+import lombok.extern.slf4j.Slf4j;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"test"})
+//@Transactional
+@Slf4j
 public class TkAdminRepositoryTest {
-	public void test() {
+	private static final String PW = "?";
+	static {
+		System.setProperty("jasypt.encryptor.password", PW);
+	}
+	@Autowired private TkAdminRepository repo;
+	
+	
+	@Test
+	void testsample() {
+		StepVerifier.create(repo.getTkAdminBySn(1L))
+		.thenConsumeWhile(a -> a.getAdminMail().equals("ADMIN"))
+		.verifyComplete();
 	}
 }
